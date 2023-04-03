0c5bb8b377b5258fae459bff4dde08ec0ad65ac1
matheushso <matheushso99@hotmail.com>
2023-04-03 11:28:52 -0300

    retornar erro de CEP inválido

---
+++ src/main/java/com/calculation/freight/domain/exception/CepInvalidoException.java
@@ -0,0 +1,10 @@
+package com.calculation.freight.domain.exception;
+
+public class CepInvalidoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CepInvalidoException(String message) {
+		super(message);
+	}
+}
