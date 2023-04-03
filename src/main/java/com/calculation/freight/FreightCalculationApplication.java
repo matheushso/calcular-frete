9f4fc25bc4235f23abc0b6dcb4b68ef9df036792
matheushso <matheushso99@hotmail.com>
2023-04-03 11:31:08 -0300

    first commit

---
+++ src/main/java/com/calculation/freight/FreightCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.calculation.freight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightCalculationApplication.class, args);
+	}
+
+}
